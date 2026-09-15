fa240d8ae469bc4542d9114affc7f0869b6c8c47
Kurt <kurtselim0000@gmail.com>
2026-09-15 16:14:28 +0800

    Add

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "change-this-before-production"
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "bestspecialproject.com",
